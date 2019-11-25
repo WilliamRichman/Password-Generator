8e49627268d04493ce12c415b881cb482a4369fd
William Richman <WRichma@mesaaz.gov>
2019-11-25 13:34:41 -0700

    found a badass thing called fromCharCode and it has lower and upper and special all in one

---
+++ script.js
@@ -0,0 +1,10 @@
+//For Williams Number Generator 
+//Link to charcode http://www.net-comber.com/charset.html
+//lower case a-z is 97 to 122
+//upper case is 65 to 90
+
+
+function getlower() {
+    return String.fromCharCode()
+}
+console.log(string.fromCharCode(97))
