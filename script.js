e35149b346fe416d197e089d8a56ae67a353241d
William Richman <WRichma@mesaaz.gov>
2019-11-26 07:14:37 -0700

    working lowercase randome selector working that was stupid hard lol

--- script.js
+++ script.js
@@ -2,9 +2,10 @@
 //Link to charcode http://www.net-comber.com/charset.html
 //lower case a-z is 97 to 122
 //upper case is 65 to 90
+//0-9 is 48 to 57
 
 
 function getlower() {
-    return String.fromCharCode()
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
-console.log(Math.random() * 26);
+console.log(getlower);
