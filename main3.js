e325b8587490dc140fd7b9e803e8f1a8d18766bc
William Richman <WRichma@mesaaz.gov>
2019-11-27 13:04:21 -0700

    Added css and js not working yet but hope soon

---
+++ main3.js
@@ -0,0 +1,17 @@
+//generate 
+function generate() {
+    //set password length and complex
+    let complex = document.getElementById('inbox').value;
+
+    //password values 
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()=+?";
+
+    let password = "";
+
+    for (var i = 0; i <= complex; i++) {
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(value.length - 1)));
+    }
+
+    //add password to text box
+    document.getElementById('display').value = password;
+}
